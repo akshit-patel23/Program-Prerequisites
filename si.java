a88dcd8ba6016eabb1b6811b38210d682f511b3e
akshit-patel23 <akshitpatel.g@gmail.com>
2025-01-08 12:54:05 +0530

    Calculate Simple Interest

---
+++ si.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class si{
+	public static void main(String args[]){
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the principal:");
+		double p=sc.nextDouble();
+		System.out.println("Enter the rate:");
+		double r=sc.nextDouble();
+		System.out.println("Enter the time:");
+		double t=sc.nextDouble();
+		
+		double si= (p*r*t)/100;
+		
+		System.out.println("Simple Interest is "+si);
+	}
+}
