bad7f4480adca19b6e0a7613c0504c10ca428c28
akshit-patel23 <akshitpatel.g@gmail.com>
2025-01-08 11:22:07 +0530

    Add Two Numbers

---
+++ addition.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class addition{
+ public static void main(String args[]){
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("Enter 2 numbers");
+	 int x=sc.nextInt();
+	 int y=sc.nextInt();
+	 System.out.println("Sum of "+x+" and "+y+" is "+(x+y));
+ }
+}
