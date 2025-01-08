6d6b77506633be7d9396f4eeb4b35cb667429621
akshit-patel23 <akshitpatel.g@gmail.com>
2025-01-08 13:20:21 +0530

    Convert Kilometers to Miles

---
+++ convert.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class convert{
+	public static void main(String args[]){
+	Scanner ak= new Scanner(System.in);
+	
+	System.out.println("Enter kilometres");
+	int km= ak.nextInt();
+	
+	System.out.println("The conversion in miles is "+(km*0.621371));
+	}
+}
